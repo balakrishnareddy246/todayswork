e4037bc1747f18bb50a6a7b019be6c6331f373da
balakrishnareddy246 <55091139+balakrishnareddy246@users.noreply.github.com>
2020-10-17 18:56:21 +0530

    Add files via upload

---
+++ 6TTE32kbqX0.go
@@ -0,0 +1,22 @@
+packge main
+ import  {
+
+           "fmt"
+
+}
+
+const {
+        a = iota
+        b = iota 
+        c = iota
+
+}
+
+func main () {
+
+
+     fmt.Pritntf("%v\n", a)
+     fmt.Printf("%v\n", b)
+     fmt.Printf("%v\n", c)
+
+}
